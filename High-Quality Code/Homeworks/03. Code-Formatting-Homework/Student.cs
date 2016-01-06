8414f060cbbb47b9ea23dd690c5dac118db49295
Vakuu <vakuu7@abv.bg>
2016-01-06 06:10:53 +0200

    Made homework 03 and 04 prepared

---
+++ High-Quality Code/Homeworks/03. Code-Formatting-Homework/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HumanStudentAndWorker
+{
+    public class Student : Human
+    {
+        private string facultyNumber;
+        public string FacultyNumber
+        {
+            get
+            {
+                return this.facultyNumber;
+            }
+            set
+            {
+                if ((value.Length<5)||(value.Length>10))
+                {
+                    throw new ArgumentOutOfRangeException("facultyNumber have to be between 5 and 10 symbols");
+                }
+                this.facultyNumber = value;
+            } 
+        }
+        public Student(string fName, string lName, string facultyNumber)
+            : base (fName,lName)
+        {
+            this.FacultyNumber = facultyNumber;
+        }
+    }
+}
---
+++ High-Quality Code/Homeworks/03. Code-Formatting-Homework/Student_reformated.cs
@@ -0,0 +1,32 @@
+﻿namespace _01_HumanStudentAndWorker
+{
+    using System;
+
+    public class Student : Human
+    {
+        private string facultyNumber;
+
+        public Student(string fName, string lName, string facultyNumber)
+            : base(fName, lName)
+        {
+            this.FacultyNumber = facultyNumber;
+        }
+
+        public string FacultyNumber
+        {
+            get
+            {
+                return this.facultyNumber;
+            }
+            set
+            {
+                if ((value.Length < 5) || (value.Length > 10))
+                {
+                    throw new ArgumentOutOfRangeException("facultyNumber have to be between 5 and 10 symbols");
+                }
+
+                this.facultyNumber = value;
+            }
+        }
+    }
+}
